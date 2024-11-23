58cc2547215123edf75f805f847c27df2b599141
Jelmo123 <j.w.golsteijn@outlook.com>
2024-11-23 08:02:29 -0500

    Task 3 completed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
+    count: 0,
     items: [], // Initialize items as an empty array
   },
   reducers: {
@@ -13,14 +14,18 @@ export const CartSlice = createSlice({
         } else {
             state.items.push({...action.payload, quantity: 1});
         }
+        state.count++;
     },
     removeItem: (state, action) => {
         state.items = state.items.filter((item) => item.name !== action.payload.name);
+        state.count -= action.payload.quantity;
     },
     updateQuantity: (state, action) => {
         let existingItem = state.items.find(item => item.name === action.payload.name)
         if (existingItem) {
+            state.count -= existingItem.quantity;
             existingItem.quantity = action.payload.quantity;
+            state.count += existingItem.quantity;
         }
     },
   },
